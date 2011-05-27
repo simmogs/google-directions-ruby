9fd4eb2f9ef712209f110252ca62025a3199b4d8
Graeme Simpson <graeme@simmo.gs>
2011-05-27 18:30:57 +0100

    Removed module nonsense

--- lib/generators/google_directions/install_generator.rb
+++ lib/generators/google_directions/install_generator.rb
@@ -1,13 +1,10 @@
-module GoogleDirections
-  module Generators
-    class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", __FILE__)
 
-      desc "Creates a Google Directions initializer for your application."
+class InstallGenerator < Rails::Generators::Base
+  source_root File.expand_path("../../templates", __FILE__)
 
-      def copy_initializer
-        template "google_directions.rb", "config/initializers/google_directions.rb"
-      end
-    end
+  desc "Creates a Google Directions initializer for your application."
+
+  def copy_initializer
+    template "google_directions.rb", "config/initializers/google_directions.rb"
   end
 end
